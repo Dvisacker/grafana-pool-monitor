d84d7cd40010b9bffcb40bc732b6b56162b5e87a
dvisacker <davidvanisacker@gmail.com>
2025-01-02 21:37:29 +0900

    chore - refactor schema

--- prisma/migrations/20250102103550_init/migration.sql
+++ prisma/migrations/20250102103550_init/migration.sql
@@ -1,18 +1,18 @@
 -- CreateTable
 CREATE TABLE "pool_reserves" (
     "time" TIMESTAMPTZ(6) NOT NULL,
-    "block_number" BIGINT NOT NULL,
+    "block_number" INTEGER NOT NULL,
     "network_id" INTEGER NOT NULL,
     "pool_address" TEXT NOT NULL,
     "token0_reserves" TEXT NOT NULL,
     "token1_reserves" TEXT NOT NULL,
 
-    CONSTRAINT "pool_reserves_pkey" PRIMARY KEY ("time","pool_address")
+    CONSTRAINT "pool_reserves_pkey" PRIMARY KEY ("block_number","network_id","pool_address")
 );
 
 -- CreateTable
 CREATE TABLE "dated_blocks" (
-    "block_number" BIGINT NOT NULL,
+    "block_number" INTEGER NOT NULL,
     "network_id" INTEGER NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
 
@@ -24,6 +24,9 @@ CREATE INDEX "pool_reserves_time_idx" ON "pool_reserves"("time" DESC);
 
 -- CreateIndex
 CREATE INDEX "pool_reserves_block_number_idx" ON "pool_reserves"("block_number");
+
+-- CreateIndex
+CREATE INDEX "pool_reserves_network_id_idx" ON "pool_reserves"("network_id");
 
 -- CreateIndex
 CREATE INDEX "pool_reserves_pool_address_idx" ON "pool_reserves"("pool_address");
